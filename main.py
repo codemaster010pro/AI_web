0c79eb1f68e764cc40692687e0325ac82d4f49cd
codemaster010pro <dhruvanshjariwala@gmail.com>
2026-08-19 23:21:31 +0530

    finally my backend is complete only fastapi wrapping is left.now i have to want for nishant to complete the frontend!!!! i will be gone for 3 to 4 days from this repo cause now i will start working with my new personal project "Edith" with my taste and my personal preference.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from schema import engine
+from schema import engine,tutor
 from langgraph.graph import StateGraph,END,START
-from nodes import quiz,evaluate,quiz_stop
+from nodes import quiz,evaluate,quiz_stop,tutor_node
+from manual_tool_node import tool_node,conditional_node
+from langgraph.checkpoint.memory import MemorySaver 
 
+memory = MemorySaver()
+#======quiz_graph======
 graph = StateGraph(engine)
 
 graph.add_node("quiz",quiz)
@@ -15,7 +19,24 @@ graph.add_conditional_edges("evaluate",quiz_stop, {
     
 })
 
-app = graph.compile()
+quiz_graph = graph.compile(checkpointer=memory)
+print(quiz_graph.get_graph().draw_mermaid())
+#======tutor_graph======
+
+Tutor_graph = StateGraph(tutor)
+
+Tutor_graph.add_node("tutor",tutor_node)
+Tutor_graph.add_node("tool_node",tool_node)
+
+Tutor_graph.add_edge(START,"tutor")
+Tutor_graph.add_conditional_edges("tutor",conditional_node, {
+    "tool_node": "tool_node",
+    "end": END
+})
+Tutor_graph.add_edge("tool_node","tutor")
+
+tutor_graph = Tutor_graph.compile(checkpointer=memory)
+print(tutor_graph.get_graph().draw_mermaid())
 
     
 
